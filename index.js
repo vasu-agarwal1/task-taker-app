9c65a3530aa7d5eeee4230d705cd6293610edf44
Vasu Agarwal <agarwal.vasu32@gmail.com>
2025-07-29 23:01:16 +0530

    connected different ejs pages

--- index.js
+++ index.js
@@ -11,8 +11,28 @@ app.use(express.static(path.join(__dirname,"public")))
 
 app.get('/', (req, res) => {
     fs.readdir(`./files`, (err, files)=> {
-        res.render("index")
+        res.render("index", {files: files})
     })
+})
+
+app.get('/files/:filename', (req, res) => {
+    fs.readFile(`./files/${req.params.filename}`, "utf-8", (err, filedata)=>{
+        res.render('show', {filename: req.params.filename, filedata: filedata})
+    })
+})
+
+app.get('/edit/:filename', (req, res) => {
+
+        res.render('edit',{filename: req.params.filename} )
+ 
+    
+})
+
+
+app.post('/create', (req, res) => {
+   fs.writeFile(`./files/${req.body.title.split(' ').join('')}.txt`, req.body.details, (err)=> {
+    res.redirect("/")
+   })
 })
 
 app.listen(3000)
